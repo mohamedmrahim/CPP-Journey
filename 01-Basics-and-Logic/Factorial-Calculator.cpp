dc5cb95f89ea7641bea41246ccfbf41152772738
Mohamed Mahmoud <mohamedmahmoudrahim@gmail.com>
2026-03-18 16:38:31 +0200

    Update Factorial-Calculator.cpp

--- 01-Basics-and-Logic/Factorial-Calculator.cpp
+++ 01-Basics-and-Logic/Factorial-Calculator.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
 
-    int number;
-    double factorial=1;
-
-  cout << "Enter a positive number \n";
+  int number;
+  double factorial = 1;
+  cout << " Enter positive number: ";
   cin >> number;
- while (number < 0) {
-        cout << "Error! Number must be positive. \n";
-        cout << "Enter a positive number \n";
-        cin >> number;
+  while (number < 0)
+  {
+    cout << " Enter positive number only: ";
+    cin >> number;
   }
-        for ( int counter =1; counter <= number ; counter++ )
-             factorial = factorial * counter ;
-        cout <<" Factorial ="<<factorial <<"\n";
 
- return 0 ;
+  for (int i = 1; i <= number; i++)
+  {
+    factorial = factorial * i;
+  }
+  cout << " Factorial= " << factorial << "\n";
+
+  return 0;
 }
